64c07123e8d913bb4885f1fa8959d0eeced5434e
davidebbo <david.ebbo@microsoft.com>
2011-10-20 12:18:17 -0700

    Initial commit

---
+++ SimpleRoslynTest/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Reflection;
+using Microsoft.CSharp;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+
+namespace SimpleRoslynTest {
+    class Program {
+        const string TestCode = @"
+using System;
+
+namespace MyNamespace {
+    public class MyTestClass {
+        public string Hello() {
+            return ""Hello!"";
+        }
+
+        public string Foo<T>() {
+            return ""Foo!"";
+        }
+    }
+}
+";
+
+        static void Main(string[] args) {
+            MeasureCompilationPerf(CompileCodeIntoAssemblyUsingRoslyn);
+            //MeasureCompilationPerf(CompileCodeIntoAssemblyUsingCodeDom);
+        }
+
+        private static void MeasureCompilationPerf(Func<string, string, Assembly> compilingMethod) {
+            compilingMethod(TestCode, "Warmup");
+
+            var start = DateTime.Now;
+            for (int i = 0; i < 5000; i++) {
+                var assembly = compilingMethod(TestCode, "TestAssembly" + i);
+                //Console.WriteLine(assembly.FullName);
+
+                var testClass = assembly.CreateInstance("MyNamespace.MyTestClass");
+                //Console.WriteLine(testClass.GetType());
+            }
+            Console.WriteLine(DateTime.Now - start);
+
+            GC.Collect();
+
+            Console.WriteLine("Press enter to end");
+            Console.ReadLine();
+        }
+
+        private static Assembly CompileCodeIntoAssemblyUsingRoslyn(string code, string assemblyName) {
+            var syntaxTree = SyntaxTree.ParseCompilationUnit(code);
+
+            var mscorlib = new AssemblyFileReference(typeof(object).Assembly.Location);
+
+            var compilationOptions = new CompilationOptions(assemblyKind: AssemblyKind.DynamicallyLinkedLibrary);
+
+            var compilation = Compilation.Create(assemblyName, compilationOptions, new[] { syntaxTree }, new[] { mscorlib });
+
+            var memStream = new MemoryStream();
+            var emitResult = compilation.Emit(memStream);
+
+            if (!emitResult.Success) {
+                foreach (Diagnostic diagnostic in emitResult.Diagnostics) {
+                    Console.WriteLine(diagnostic.Info.ToString());
+                }
+            }
+
+            var assembly = Assembly.Load(memStream.GetBuffer());
+            return assembly;
+        }
+
+        private static Assembly CompileCodeIntoAssemblyUsingCodeDom(string code, string assemblyName) {
+            var codeDomProvider = new CSharpCodeProvider();
+
+            CompilerResults results = codeDomProvider.CompileAssemblyFromSource(new CompilerParameters(), code);
+            if (results.Errors.HasErrors) {
+                foreach (CompilerError error in results.Errors) {
+                    Console.WriteLine(error.ErrorText);
+                }
+            }
+
+            return results.CompiledAssembly;
+        }
+    }
+}
---
+++ SimpleRoslynTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleRoslynTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SimpleRoslynTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5494b8bf-14b2-41ec-bbc0-7e31020096fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
